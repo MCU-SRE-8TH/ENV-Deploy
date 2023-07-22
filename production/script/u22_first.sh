114ea4f04131f4642fcdf447190bdf10911cebb3
t985026 <9305119+t985026@users.noreply.github.com>
2023-07-22 12:24:58 +0800

    Update u22_first.sh

--- production/script/u22_first.sh
+++ production/script/u22_first.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+sudo apt install -y ntp ntpdate
+
+# 更改時區
+sudo timedatectl set-timezone Asia/Taipei
+sudo timedatectl set-ntp yes
+sudo ntpdate time.stdtime.gov.tw
+sudo hwclock -w
+
 echo '120.96.143.50 rke 
 120.96.143.43 pm1 
 120.96.143.44 pw1 
